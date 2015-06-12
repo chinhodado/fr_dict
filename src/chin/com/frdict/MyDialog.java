a5ba877709aa8e8286c2cdd7bc6f3251873a2e17
Chin <chin.bimbo@gmail.com>
2015-06-12 00:37:09 -0400

    search word directly using the keyboard, instead of using the button. Remove the button.

--- src/chin/com/frdict/MyDialog.java
+++ src/chin/com/frdict/MyDialog.java
@@ -12,13 +12,15 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MyDialog extends Activity {
@@ -33,7 +35,6 @@ public class MyDialog extends Activity {
         setContentView(R.layout.dialog);
 
         final EditText edt = (EditText) findViewById(R.id.dialog_edt);
-        Button btn = (Button) findViewById(R.id.dialog_btn);
         View top = (View) findViewById(R.id.dialog_top);
         final WebView webView = (WebView) findViewById(R.id.webView1);
         webView.setWebViewClient(new WebViewClient() {
@@ -45,119 +46,123 @@ public class MyDialog extends Activity {
 
         myDialog = MyDialog.this;
 
-        btn.setOnClickListener(new OnClickListener() {
+        edt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                final String str = edt.getText().toString();
-                if (str.length() > 0) {
-                    new AsyncTask<Void, Void, String>(){
-                        boolean exceptionOccurred = false;
-                        @Override
-                        protected String doInBackground(Void... params) {
-                            String html = null;
-                            try {
-                                html = Jsoup.connect("http://en.wiktionary.org/w/index.php?title=" + str + "&printable=yes")
-                                        .ignoreContentType(true).execute().body();
-                            } catch (IOException e) {
-                                html = "Error getting word from Wiktionary. Probably 404-ed";
-                                exceptionOccurred = true;
-                                e.printStackTrace();
-                            }
-                            return html;
-                        }
-
-                        @Override
-                        protected void onPostExecute(String html) {
-                            try {
-                                if (exceptionOccurred) {
-                                    webView.loadDataWithBaseURL("", html, "text/html", "UTF-8", "");
-                                    return;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    final String str = edt.getText().toString();
+                    if (str.length() > 0) {
+                        new AsyncTask<Void, Void, String>(){
+                            boolean exceptionOccurred = false;
+                            @Override
+                            protected String doInBackground(Void... params) {
+                                String html = null;
+                                try {
+                                    html = Jsoup.connect("http://en.wiktionary.org/w/index.php?title=" + str + "&printable=yes")
+                                            .ignoreContentType(true).execute().body();
+                                } catch (IOException e) {
+                                    html = "Error getting word from Wiktionary. Probably 404-ed";
+                                    exceptionOccurred = true;
+                                    e.printStackTrace();
                                 }
+                                return html;
+                            }
 
-                                // parse and do some initial cleaning of the DOM
-                                Document doc = Jsoup.parse(html);
-                                Element content = doc.select("#mw-content-text").first();
-                                content.select("script").remove();               // remove <script> tags
-                                content.select("noscript").remove();             // remove <noscript> tags
-                                content.select("#toc").remove();                 // remove the table of content
-                                removeComments(content);                         // remove comments
-
-                                // parse for the content of the French section, if it exist
-                                Elements children = content.children();
-                                boolean frenchFound = false;
-                                boolean frenchEndReached = false;
-                                boolean isCurrentlyEtymology = false;
-                                boolean isCurrentlyPronunciation = false;
-                                Elements frenchCollection = new Elements();
-                                Elements etymologyCollection = new Elements();
-                                Elements pronunciationCollection = new Elements();
-                                for (Element elem : children) {
-                                    if (!frenchFound) {
-                                        if (elem.tagName().equals("h2") && elem.text().equals("French")) {
-                                            frenchFound = true;
-                                        }
-                                        else {
-                                            elem.remove();
-                                        }
+                            @Override
+                            protected void onPostExecute(String html) {
+                                try {
+                                    if (exceptionOccurred) {
+                                        webView.loadDataWithBaseURL("", html, "text/html", "UTF-8", "");
+                                        return;
                                     }
-                                    else {
-                                        if (!elem.tagName().equals("h2") && !(elem.tagName().equals("h3") && elem.text().equals("External links")) && !frenchEndReached) {
-                                            // get etylmology and pronunciation sections so that we can move them to the back
-                                            // of the page later, instead of having them at the beginning of the page
-                                            if (elem.tagName().equals("h3") && elem.text().equals("Etymology")) {
-                                                isCurrentlyEtymology = true;
-                                                isCurrentlyPronunciation = false;
-                                                etymologyCollection.add(elem);
-                                            }
-                                            else if (elem.tagName().equals("h3") && elem.text().equals("Pronunciation")) {
-                                                isCurrentlyPronunciation = true;
-                                                isCurrentlyEtymology = false;
-                                                pronunciationCollection.add(elem);
-                                            }
-                                            else if (elem.tagName().equals("h3")) {
-                                                // something other than etymology and pronunciation
-                                                isCurrentlyEtymology = false;
-                                                isCurrentlyPronunciation = false;
-                                                frenchCollection.add(elem);
-                                            }
-                                            else if (isCurrentlyEtymology) {
-                                                isCurrentlyPronunciation = false;
-                                                etymologyCollection.add(elem);
-                                            }
-                                            else if (isCurrentlyPronunciation) {
-                                                isCurrentlyEtymology = false;
-                                                pronunciationCollection.add(elem);
+
+                                    // parse and do some initial cleaning of the DOM
+                                    Document doc = Jsoup.parse(html);
+                                    Element content = doc.select("#mw-content-text").first();
+                                    content.select("script").remove();               // remove <script> tags
+                                    content.select("noscript").remove();             // remove <noscript> tags
+                                    content.select("#toc").remove();                 // remove the table of content
+                                    removeComments(content);                         // remove comments
+
+                                    // parse for the content of the French section, if it exist
+                                    Elements children = content.children();
+                                    boolean frenchFound = false;
+                                    boolean frenchEndReached = false;
+                                    boolean isCurrentlyEtymology = false;
+                                    boolean isCurrentlyPronunciation = false;
+                                    Elements frenchCollection = new Elements();
+                                    Elements etymologyCollection = new Elements();
+                                    Elements pronunciationCollection = new Elements();
+                                    for (Element elem : children) {
+                                        if (!frenchFound) {
+                                            if (elem.tagName().equals("h2") && elem.text().equals("French")) {
+                                                frenchFound = true;
                                             }
                                             else {
-                                                isCurrentlyPronunciation = false;
-                                                isCurrentlyEtymology = false;
-                                                frenchCollection.add(elem);
+                                                elem.remove();
                                             }
                                         }
                                         else {
-                                            frenchEndReached = true;
-                                            break;
+                                            if (!elem.tagName().equals("h2") && !(elem.tagName().equals("h3") && elem.text().equals("External links")) && !frenchEndReached) {
+                                                // get etylmology and pronunciation sections so that we can move them to the back
+                                                // of the page later, instead of having them at the beginning of the page
+                                                if (elem.tagName().equals("h3") && elem.text().equals("Etymology")) {
+                                                    isCurrentlyEtymology = true;
+                                                    isCurrentlyPronunciation = false;
+                                                    etymologyCollection.add(elem);
+                                                }
+                                                else if (elem.tagName().equals("h3") && elem.text().equals("Pronunciation")) {
+                                                    isCurrentlyPronunciation = true;
+                                                    isCurrentlyEtymology = false;
+                                                    pronunciationCollection.add(elem);
+                                                }
+                                                else if (elem.tagName().equals("h3")) {
+                                                    // something other than etymology and pronunciation
+                                                    isCurrentlyEtymology = false;
+                                                    isCurrentlyPronunciation = false;
+                                                    frenchCollection.add(elem);
+                                                }
+                                                else if (isCurrentlyEtymology) {
+                                                    isCurrentlyPronunciation = false;
+                                                    etymologyCollection.add(elem);
+                                                }
+                                                else if (isCurrentlyPronunciation) {
+                                                    isCurrentlyEtymology = false;
+                                                    pronunciationCollection.add(elem);
+                                                }
+                                                else {
+                                                    isCurrentlyPronunciation = false;
+                                                    isCurrentlyEtymology = false;
+                                                    frenchCollection.add(elem);
+                                                }
+                                            }
+                                            else {
+                                                frenchEndReached = true;
+                                                break;
+                                            }
                                         }
                                     }
-                                }
 
-                                // put pronunciation and etymology sections to the back
-                                frenchCollection.addAll(pronunciationCollection);
-                                frenchCollection.addAll(etymologyCollection);
+                                    // put pronunciation and etymology sections to the back
+                                    frenchCollection.addAll(pronunciationCollection);
+                                    frenchCollection.addAll(etymologyCollection);
 
-                                //webView.getSettings().setJavaScriptEnabled(true);
-                                if (frenchFound) {
-                                    webView.loadDataWithBaseURL("http://en.wiktionary.org/w/", frenchCollection.toString(), "text/html", "UTF-8", "");
+                                    //webView.getSettings().setJavaScriptEnabled(true);
+                                    if (frenchFound) {
+                                        webView.loadDataWithBaseURL("http://en.wiktionary.org/w/", frenchCollection.toString(), "text/html", "UTF-8", "");
+                                    }
+                                    else {
+                                        webView.loadDataWithBaseURL("", "Not a French word.", "text/html", "UTF-8", "");
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
                                 }
-                                else {
-                                    webView.loadDataWithBaseURL("", "Not a French word.", "text/html", "UTF-8", "");
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
                             }
-                        }
-                    }.execute();
+                        }.execute();
+                    }
+                    return true;
                 }
+                return false;
             }
         });
 
